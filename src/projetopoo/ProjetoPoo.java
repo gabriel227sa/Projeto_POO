8f9b2cc2d09710ea9a465b779fbe444535763b5c
Erick-Faster <52637908+Erick-Faster@users.noreply.github.com>
2019-07-29 03:41:09 -0300

    Update ProjetoPoo.java

--- src/projetopoo/ProjetoPoo.java
+++ src/projetopoo/ProjetoPoo.java
@@ -6,7 +6,9 @@
  *
  *
  *
- *  @author Gabriel Araújo
+ *  @authors:
+ *  Erick F da Silva 
+ *  Gabriel Araujo
  *
  *
  *
@@ -19,11 +21,11 @@ import java.util.Scanner;
 public class ProjetoPoo {
     public static void main(String[] args) {        
         
-        // Os herois (alunos) são guardados aqui.
+        // Os herois (alunos) sao guardados aqui.
         Herois herois = new Herois(); 
-        // O menu inicial para a escolha do perfil de acesso é iniciado.
+        // O menu inicial para a escolha do perfil de acesso iniciado.
         PerfisDeAcesso acesso = new PerfisDeAcesso();
-        // A base com os slots são passado por parâmetro.
+        // A base com os slots sÃ£o passado por parÃ¢metro.
         acesso.menuDeAcesso(herois);               
     }
 }

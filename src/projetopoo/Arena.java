94eb0c6ef05d708d38b803815569c2c606ac430d
Erick-Faster <52637908+Erick-Faster@users.noreply.github.com>
2019-07-29 03:34:17 -0300

    Update Arena.java

--- src/projetopoo/Arena.java
+++ src/projetopoo/Arena.java
@@ -7,6 +7,7 @@ package projetopoo;
 
 import java.util.Scanner;
 
+
 public class Arena {
 	Scanner scanner = new Scanner(System.in);
 	Character jogador; //Atributo que contem jogador participante da Arena
@@ -26,37 +27,58 @@ public class Arena {
 	//Turno do Jogador. Sempre se inicia primeiro, com uma palavra atribuida previamente
 	public void TurnoJogador(Palavra word) {
 		System.out.println("Turno do Jogador!!\n");
-		System.out.println("O que significa \""+word.getGenero()+" "+word.getPalavra()+"?");
-		String x = scanner.next(); //Jogador introduz traducao da palavra
+		espera(1000);
+		System.out.println("O que significa \""+word.getGenero()+" "+word.getPalavra()+"\"?");
+		System.out.printf(">>");
+		String x = (scanner.next()).toLowerCase(); //Jogador introduz traducao da palavra
 		if (x.equals(word.getTraducao())) { //Caso a palavra esteja correta
-			System.out.println("Resposta Correta!");
+			System.out.println("\nResposta Correta! "+jogador.getNome()+" consegue atingir o inimigo");
+			espera(1000);
 			dano = jogador.getATK()+((int)Math.random()*10); // Inflige dano ao oponente baseado no ATK
 		}
 		else { //Caso a palavra esteja errada
-			System.out.println("Resposta Errada!");
+			System.out.println("\nResposta Errada! O inimigo resiste ao ataque");
+			espera(1000);
 			dano = ((int)Math.random()*3)+1; //Dano minimo de 1 a 3 sera infligido ao oponente
 		}
 		inimigo.tomaDano(dano); //Dano infligido ao inimigo
 		inimigo.verHP(); // Verificacao do HP inimigo
 		inimigo.verificaStatus(); //Verificacao da condicao de derrota do inimigo
+		espera(1000);
 	}
 	
 	//Turno do inimigo. Uma palavra sempre eh atribuida externamente
 	public void TurnoInimigo(Palavra word) {
 		System.out.println("Turno do Inimigo!!\n");
-		System.out.println("O que significa \""+word.getPalavra()+"?");
-		String x = scanner.next(); //Jogador introduz palavra
+		espera(1000);
+		System.out.println("O que significa \""+word.getGenero()+" "+word.getPalavra()+"\"?");
+		System.out.printf(">>");
+		String x = (scanner.next()).toLowerCase(); //Jogador introduz palavra
+		
 		if (x.equals(word.getTraducao())) { //Caso palavra esteja correta
-			System.out.println("Resposta Correta!");
-			dano = ((int)Math.random()*3)+1; //Dano levado sera minimo, de 1 a 3
+			System.out.println("\nResposta Correta! "+jogador.getNome()+" consegue desviar do golpe");
+			espera(1000);
 		}
 		else { //Caso a palavra esteja errada
-			System.out.println("Resposta Errada!");	
+			System.out.println("\nResposta Errada! "+jogador.getNome()+" foi atingido pelo inimigo");
+			espera(1000);
 			dano = inimigo.getATK()+((int)Math.random()*10); //Dano tomado considera ATK do inimigo
+			jogador.tomaDano(dano);
 		}
-		jogador.tomaDano(dano); //Subtracao do HP no dano levado
+		; //Subtracao do HP no dano levado
 		jogador.verHP(); //Verificacao do HP do jogador
 		jogador.verificaStatus(); //Verificacao se jogador nao sofreu KO (HP <= 0 )
+		espera(1000);
+	}
+	
+	//Permite que o codigo aguarde antes de prosseguir com a sua execucao
+	public void espera(int mili) {
+		try {
+		    Thread.sleep(mili);
+		} catch (InterruptedException e) {
+		    System.err.format("ERRO", e);
+		}
+		
 	}
 	
 }

eaeb9e748825c8b8bbe6c2c77cb780dd01b1a0c7
Erick-Faster <52637908+Erick-Faster@users.noreply.github.com>
2019-07-29 03:35:46 -0300

    Update Character.java

--- src/projetopoo/Character.java
+++ src/projetopoo/Character.java
@@ -7,6 +7,7 @@ package projetopoo;
 
 public class Character extends Monstro { //Heranca de monstros. Herois tambem sao monstros por dentro =)
 	int nivel; //nivel atual do heroi
+	int maxNivel = 0;
 	private static boolean derrota; //Condicao que determina se jogador foi derrotado
 	
 	public Character() {
@@ -29,13 +30,41 @@ public class Character extends Monstro { //Heranca de monstros. Herois tambem sa
 		Character.derrota = derrota;
 	}
 
+	public int getMaxNivel() {
+		return maxNivel;
+	}
+
+	public void setMaxNivel(int maxNivel) {
+		this.maxNivel = maxNivel;
+	}
+
 	//Da descricao detalhada de heroi
+	
+	@Override
 	public void Descricao() {
 		System.out.println(this.getNome()+":");
 		System.out.println("MaxHP: "+this.getMaxHP());
 		System.out.println("ATK: "+this.getATK());
 		System.out.println("Nivel: "+this.getNivel());
-		System.out.println("EXP: "+this.getEXP());
 		System.out.println();
 	}
+	
+	@Override
+	public void verificaStatus() {
+		if (this.getHP() <= 0) {
+			Character.setDerrota(true);
+		}
+	}
+	
+	public void Score(int score) {
+		if (score > maxNivel) {
+			maxNivel = score;
+		}
+	}
+	
+	public void reset() {
+		this.HP = MaxHP;
+		this.nivel = 1;
+	}
+	
 }

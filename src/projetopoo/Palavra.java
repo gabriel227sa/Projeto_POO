c5bcb9be88bb11fed8092bb4893b25992abc5589
Erick-Faster <52637908+Erick-Faster@users.noreply.github.com>
2019-07-29 03:55:59 -0300

    Update Palavra.java

--- src/projetopoo/Palavra.java
+++ src/projetopoo/Palavra.java
@@ -1,56 +1,50 @@
-/*
- * 
- */
+
 package projetopoo;
 
-import java.util.Scanner;
-
-public class PerfisDeAcesso {
-
-    Scanner scan = new Scanner(System.in);
-    private int numero;
-    private String senha;
-
-    // Metodo que imprime o menu inicial.
-    public void menuDeAcesso(Herois herois) {
-        System.out.println("\n           =============================================");
-        System.out.println("           |     Perfil de Acesso Aluno ou professor?  |");
-        System.out.println("           |                                           |");
-        System.out.println("           |     1 - Professor.                        |");
-        System.out.println("           |     2 - Aluno.                            |");
-        System.out.println("           |     0 - Fechar Programa.                  |");
-        System.out.printf("           =============================================\n>> ");
-        numero = scan.nextInt();
-        // Eh chamado o metodo para o acesso ao perfil de aluno ou professor.
-        usuario(herois);
-    }
-
-    // Metodo que retorna o numero de acesso escolhido pelo usuario.
-    public int retornaNumero() {
-        return numero;
-    }
-
-    // Metodo que recebe os herois e chama o perfil de acordo com a escolha do usuario.
-    public void usuario(Herois herois) {
-        if (this.numero == 1) {
-            // Eh criado um novo perfil de professor.            
-            System.out.printf("**Perfil restrito! Digite a senha para continuar :\n>> ");
-            senha = scan.next();
-            //Para o acesso ao perfil de professor deve-se acertar a senha.
-            if (senha.equals("PROFESSOR")) {
-                new Professor(herois);
-            } else {
-                System.out.println("**Escolha uma opÃ§Ã£o vÃ¡lida!**");
-                menuDeAcesso(herois);
-            }
-        } else if (this.numero == 2) {
-            // Eh criado um novo perfil de aluno.
-            new Aluno(herois);
-        } else if (this.numero == 0) {
-            System.exit(0);
-        } else {
-            System.out.println("**Escolha uma opÃ§Ã£o vÃ¡lida!**");
-            menuDeAcesso(herois);
-        }
-    }
+public class Palavra {
+	private String palavra;
+	private String traducao;
+	private String genero;
+	private int dificuldade;
+	
+	public Palavra(String p, String t, String g, int d){
+		this.palavra = p;
+		this.traducao = t;
+		this.genero = g;
+		this.dificuldade = d;
+	}
+	
+	public String getPalavra() {
+		return palavra;
+	}
+	public void setPalavra(String palavra) {
+		this.palavra = palavra;
+	}
+	public String getTraducao() {
+		return traducao;
+	}
+	public void setTraducao(String traducao) {
+		this.traducao = traducao;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	public int getDificuldade() {
+		return dificuldade;
+	}
+	public void setDificuldade(int dificuldade) {
+		this.dificuldade = dificuldade;
+	}
+	
+	public void Descricao() {
+		System.out.println(this.getPalavra()+":");
+		System.out.println(this.getTraducao());
+		System.out.println(this.getGenero());
+		System.out.println(this.getDificuldade());
+		System.out.println();
+	}
+	
 }

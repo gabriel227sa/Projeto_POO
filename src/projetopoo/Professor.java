441452a1591532d6a6939bd3590125c7c89e7afa
Erick-Faster <52637908+Erick-Faster@users.noreply.github.com>
2019-07-31 02:28:30 -0300

    Update Professor.java

--- src/projetopoo/Professor.java
+++ src/projetopoo/Professor.java
@@ -22,8 +22,8 @@ public class Professor extends PerfisDeAcesso {
             System.out.println("           |                              |");
             System.out.println("           |     1 - Adicionar alunos.    |");
             System.out.println("           |     2 - Remover alunos.      |");
-            System.out.println("           |     3 - Adicionar palavras.  |");
-            System.out.println("           |     4 - Remover palavras.    |");
+            System.out.println("           |     3 - Selecionar idioma.   |");
+            System.out.println("           |     4 - Mudar Senha          |");
             System.out.println("           |     5 - Voltar.              |");
             System.out.println("           |     0 - Fechar Programa.     |");
             System.out.printf("           ================================\n>> ");            
@@ -36,10 +36,10 @@ public class Professor extends PerfisDeAcesso {
                     removerAlunos(herois);
                     break;
                 case 3:
-                    adicionarPalavras(herois);
+                    selecionarIdioma(herois);
                     break;
                 case 4:
-                    adicionarPalavras(herois);
+                    mudarSenha(herois);
                     break;
                 case 5: {
                     menuDeAcesso(herois);
@@ -51,7 +51,7 @@ public class Professor extends PerfisDeAcesso {
         } while (escolha != 0);
     }
 
-    // Metodo que adiciona os alunos aos slots de herois.
+	// Metodo que adiciona os alunos aos slots de herois.
     public void adicionarAlunos(Herois herois) {
         char continua = 'S';
         //Imprime todos os slots de heroi que estao disponiveis ou nao.
@@ -78,10 +78,9 @@ public class Professor extends PerfisDeAcesso {
         herois.ListaChar();
     }
 
-    // Metodo que adciona palavras baseada no idioma escolhido.
-    //arrumar.....
-    public void adicionarPalavras(Herois herois) {
-        do {
+    // Metodo que seleciona o idioma
+    public void selecionarIdioma(Herois herois) {
+        
             System.out.println("\n           ================================");
             System.out.println("           |     Escolha o idioma:        |");
             System.out.println("           |                              |");
@@ -95,45 +94,32 @@ public class Professor extends PerfisDeAcesso {
             escolha = scan.nextInt();
             switch (escolha) {
                 case 1:
-                    // exibir as palavras jÃ¡ adicionadas
-                    //arrumar dicionario para as linguagem
+                    Dicionario.setLingua("ingles");
                     break;
                 case 2:
-                    // exibir as palavras jÃ¡ adicionadas
-                    //arrumar dicionario para as linguagem
+                	Dicionario.setLingua("alemao");
                     break;
                 case 3:
-                    // exibir as palavras jÃ¡ adicionadas
-                    //arrumar dicionario para as linguagem
+                	Dicionario.setLingua("espanhol");
                     break;
                 case 4:
-                    // exibir as palavras jÃ¡ adicionadas
-                    //arrumar dicionario para as linguagem
+                	Dicionario.setLingua("frances");
                     break;
                 case 5: {
-                    menuDeAcesso(herois);
                     break;
                 }
                 default:
                     System.exit(0);
             }
-        } while (escolha != 0);
+            System.out.println("Idioma configurado para \""+Dicionario.getLingua()+"\".");
+        
     }
 
     // Metodo que remove palavra baseada no idioma escolhido
     //arrumar....
-    public void removerPalavras() {
-        // criar funcao para remover palavras
-        System.out.println("\n           ================================");
-        System.out.println("           |     Escolha o idioma:        |");
-        System.out.println("           |                              |");
-        System.out.println("           |     1 - Ingles               |");
-        System.out.println("           |     2 - Alemao               |");
-        System.out.println("           |     3 - Espanhol             |");
-        System.out.println("           |     4 - Frances              |");
-        System.out.println("           |     5 - Voltar.              |");
-        System.out.println("           |     0 - Fechar Programa.     |");
-        System.out.printf("           ================================\n>> ");
-        escolha = scan.nextInt();
+    public void mudarSenha(Herois herois) {
+        System.out.println("Digite a nova senha de perfil de acesso de professor:");
+        String novaSenha = scan.next();
+        acesso = novaSenha;
     }
 }
